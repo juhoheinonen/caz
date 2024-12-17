a776211fcd9fd9a3a64f318123823fca35cf0929
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2024-12-17 20:22:45 +0200

    Luo peliruudukko osittain

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,46 @@ import { Peliruutu, Ruututyyppi } from "./tyypit";
 const ruudukkoLeveys: number = 60;
 const ruudukkoKorkeus: number = 60;
 const button = document.querySelector("#painikeAloitaPeli");
+const peliruudukko: Peliruutu[][] = [];
+
 
 if (!button) {
-  throw new Error("Button not found");
+  throw new Error("Painiketta ei löytynyt");
 }
 button.addEventListener("click", (event) => {
   console.log("Painiketta painettu");
+  aloitaPeli();
 });
 
-function aloitaPeli() {
-  const taulukko = document.getElementById("taulukkoRuudukko");
-  for (let x = 0; x < ruudukkoLeveys; x++) {
 
+function aloitaPeli() {
+  alustaPeliruudukko();
+  piirraRuudukko();
+}
+
+function alustaPeliruudukko() {
+  for (let x = 0; x < ruudukkoLeveys; x++) {
+    for (let y = 0; y < ruudukkoKorkeus; y++) {
+      peliruudukko[x][y] = {
+        tyyppi: Ruututyyppi.Tyhja,
+      };
+      // peliruudukko.push(Array(30).fill(x));
+    }
   }
 }
+
+function piirraRuudukko() {
+  const taulukko = document.getElementById("taulukkoRuudukko");
+  if (!taulukko) {
+    throw new Error("Taulukkoa ei löytynyt");
+  }
+  for (let y = 0; y < ruudukkoKorkeus; y++) {
+    const rivi = document.createElement("tr");
+    taulukko.append(rivi);
+    for (let x = 0; x < ruudukkoLeveys; x++) {
+      
+    }
+  }
+}
+
+
